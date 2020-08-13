ce9c4517f66c752632aa3b601d75e3ea8e8e5036
ShyamSundar Prajapati <shyamsundar@avyakinfoways.com>
2020-08-13 15:52:01 +0530

    Add project files.

---
+++ ASG/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASG.MvcApplication" Language="C#" %>
---
+++ ASG/Models/Registration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ASG.Models
+{
+    public class Registration
+    {
+        public string Name { get; set; }
+       public String Email { get; set; }
+       public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        [DisplayName("I Read and Accept")]
+        public bool IReadAndAccept { get; set; }
+    }
+}
